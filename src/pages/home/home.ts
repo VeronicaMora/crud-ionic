9fe6fa4da18d579c64d2b50ca85a279e5ff878d3
VeronicaMora <veronicaymv@hotmail.com>
2018-10-27 00:05:38 -0400

    list of notes ui

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NotesPage } from '../notes/notes'
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,38 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    this.initializeItems();
+  }
 
+  searchQuery: string = '';
+  notes: string[];
+
+  initializeItems() {
+    this.notes = [
+      
+    ];
+  }
+
+
+  getItems(ev) {
+    var val = ev.target.value;
+    if (val && val.trim() != '') {
+      this.notes = this.notes.filter((item) => {
+        return (notes.(falta).toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+    else if(!val || val.trim() == ''){
+      this.initializeItems()
+    }
+  }
+
+  onCancel(){
+    this.initializeItems()
+  }
+
+  createNote(){
+    this.navCtrl.push(NotesPage)
+    console.log('asd')
   }
 
 }
