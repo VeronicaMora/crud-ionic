4938d86e7c12e1cf6b20315f639c7fe7734425a5
marigalvis6 <mariapgalvisd@gmail.com>
2018-10-31 21:46:09 -0400

    ui register

---
+++ src/pages/register/register.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { LoginPage } from '../login/login';
+
+@IonicPage()
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html',
+})
+export class RegisterPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+  }
+
+  doSignup(){
+    this.navCtrl.setRoot(HomePage);
+  }
+
+  goHome(){
+    this.navCtrl.setRoot(LoginPage);
+  }
+
+}
